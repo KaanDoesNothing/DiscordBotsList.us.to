1de16cca66a897ef4f41b78207eb62bde4c284fd
KaanDoesNothing <kaanyt123@gmail.com>
2021-01-04 10:28:27 +0100

    Fixed a flaw(Thanks jayy)

--- src/routes/api/bot/index.js
+++ src/routes/api/bot/index.js
@@ -10,7 +10,9 @@ app.use("/comments", require("./comments"));
 
 app.get("/:id", async (req, res) => {
     const bot = await db.get("bots").findOne({bot_id: req.params.id});
-    const fixedBot = await fixBot(bot);
+    let fixedBot = await fixBot(bot);
+    
+    delete fixedBot.api_key;
 
     return res.json({bot: fixedBot});
 });
