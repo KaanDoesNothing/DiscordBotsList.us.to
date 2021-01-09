8072fe9add2cd518f8c06af422be9a7c95d6e4ea
KaanDoesNothing <kaanyt123@gmail.com>
2021-01-09 13:56:19 +0100

    Cleaning some code up

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
     }
 
     res.locals.query = req.query;
+    res.locals.params = req.params;
     res.locals.darkMode = req.session.darkMode;
 
     next();
@@ -57,76 +58,6 @@ app.get("/switch_theme", async (req, res) => {
 
 app.use((req, res) => {
     return res.render("client", {client});
-});
-// app.use("/admin", isLoggedIn, isModerator, require("./routes/admin"));
-// app.use("/bot", require("./routes/bot"));
-// app.use("/api", require("./routes/api/index"));
-
-app.get("/", async (req, res) => {
-    let search = req.query.search;
-    
-    const bots = await db.get("bots").find({verified: true}, {sort: {added: -1}});
-    let finalBots = await Promise.all(bots.map(bot => fixBot(bot)));
-
-    if(search && search.length > 0) {
-        finalBots = finalBots.filter(bot => bot.user.username.startsWith(search));
-    }
-
-    res.render("home", {bots: finalBots, search});
-});
-
-// app.get("/bots/json", async (req, res) => {
-//     let search = req.query.search;
-    
-//     const bots = await db.get("bots").find({verified: true});
-//     let finalBots = await Promise.all(bots.map(bot => fixBot(bot)));
-
-//     if(search && search.length > 0) {
-//         finalBots = finalBots.filter(bot => bot.user.username.startsWith(search));
-//     }
-
-//     return res.json({bots});
-// });
-
-app.get("/add", isLoggedIn, (req, res) => {
-    res.render("add");
-});
-
-app.post("/add", isLoggedIn, async (req, res) => {
-    let body = req.body;
-
-    let finalBody = {...body, owner_id: res.locals.session._id, likes: 0, verified: false, added: Date.now()};
-
-    const isValidated = botSchema.validate(finalBody);
-
-    if(isValidated.error) {
-        return res.json({error: isValidated.error.details[0].message});
-    }
-
-    try {
-        await client.users.fetch(finalBody.bot_id);
-    }catch(err) {
-        return res.json({error: "Invalid bot id."});
-    }
-
-    let alreadyExists = await db.get("bots").findOne({bot_id: finalBody.bot_id});
-
-    if(alreadyExists) return res.json({error: "Bot already exists."});
-
-    db.get("bots").insert(finalBody).then(() => {
-        sendBotAdded(finalBody.owner_id, finalBody.bot_id);
-        return res.json({msg: "Your bot has been added."});
-    }).catch(err => {
-        return res.json({error: "Couldn't add the bot to the database."});
-    });
-});
-
-app.get("/docs", (req, res) => {
-    res.render("docs");
-});
-
-app.get("/csr", (req, res) => {
-    res.render("client");
 });
 
 app.listen(config.website.port || 4000);
